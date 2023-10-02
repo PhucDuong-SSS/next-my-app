d86b91269445086a738ae4a00046dee6bcf76dfb
phuc-2561 <duong.ngoc.phuc@sun-asterisk.com>
2023-10-02 22:38:31 +0700

    add style

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,27 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+
+import Link from "next/link";
+import styles from "../styles/home.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.title}>
-          Home page
-          <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus fuga inventore velit ea sapiente aperiam! Tenetur, doloribus. Eum ipsum id possimus, incidunt, modi iure quae, quidem debitis quia odio molestiae!</p>
-          <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus fuga inventore velit ea sapiente aperiam! Tenetur, doloribus. Eum ipsum id possimus, incidunt, modi iure quae, quidem debitis quia odio molestiae!</p>
-      </div>
-    </main>
-  )
+    <div className={styles.container}>
+      <h1 className={styles.title}>Homepage</h1>
+      <p className={styles.text}>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate
+        ullam qui, provident sequi odit aliquam repudiandae inventore nemo
+        excepturi officiis distinctio, molestias hic veritatis facilis?
+        Exercitationem quia hic praesentium sed!
+      </p>
+      <p className={styles.text}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, eius
+        aperiam assumenda ducimus necessitatibus exercitationem consequuntur
+        iure dolore doloremque, doloribus molestiae dolores reiciendis optio
+        corrupti voluptas veritatis, suscipit perferendis. Dolores.
+      </p>
+      <Link href="/dashboard">
+        <div className={styles.btn}>See List Nine Dev</div>
+      </Link>
+    </div>
+  );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,21 +1,17 @@
-'use client'
-import Link from "next/link"
-import {useRouter} from 'next/navigation'
+"use client";
+import Link from "next/link";
 
 const Navbar = () => {
-
-    const router = useRouter()
-return (
-  <nav>
-    <div className="logo">
+  return (
+    <main>
+      <div className="logo">
         <h1>Test</h1>
-    </div>
-    <Link href="/">Home</Link>
-    <Link href="/about">About</Link>
-    <Link href="/dashboard">Dashboard</Link>
-    <button onClick={()=>router.push('dashboard')} >click to dashboard</button>
-  </nav>
-)
-}
+      </div>
+      <Link href="/">Home</Link>
+      <Link href="/about">About</Link>
+      <Link href="/dashboard">Dashboard</Link>
+    </main>
+  );
+};
 
-export default Navbar
+export default Navbar;
